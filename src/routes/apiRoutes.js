f0e1df53b6013245064314626929c38587139cee
guinegrijo <guilhermenegrijo@gmail.com>
2025-09-10 09:42:36 -0300

    Retirada de VerifyJWT de rota get

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -19,7 +19,7 @@ router.delete('/organizador/:id', orgController.deleteOrg);
 const eventoController = require("../controller/eventoController")
 
 router.post('/evento', upload.single("imagem"), eventoController.createEvento)
-router.get('/evento', verifyJWT, eventoController.getAllEventos)
+router.get('/evento', eventoController.getAllEventos)
 router.put('/evento', eventoController.updateEvento)
 router.delete('/evento/:id', eventoController.deleteEvento)
 router.get('/evento/data', eventoController.getEventosPorData)
